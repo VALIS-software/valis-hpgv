7c59cbdb8ce9b1abb0f6a2c7ac82e7989cbc0642
George Corney <haxiomic@gmail.com>
2018-09-28 18:44:21 +0100

    Use manifest path

--- examples/typescript/src/App.ts
+++ examples/typescript/src/App.ts
@@ -1,10 +1,13 @@
 import * as ReactDOM from "react-dom";
 import { GenomeBrowser } from "genome-browser";
 
-let browser = new GenomeBrowser({
-    panels: [{ location: { contig: 'chr1', x0: 0, x1: 249e6 } }],
-    tracks: [{ model: { name: 'GRCh38', type: 'sequence' }, heightPx: 100 }],
-});
+let browser = new GenomeBrowser(
+    './manifest.json',
+    {
+        panels: [{ location: { contig: 'chr1', x0: 0, x1: 249e6 } }],
+        tracks: [{ model: { name: 'GRCh38', type: 'sequence' }, heightPx: 100 }],
+    }
+);
 
 let container = document.getElementById('root');
 

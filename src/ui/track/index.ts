92aa8ed375e70d50844edb3738c057d2b0afd30e
George Corney <haxiomic@gmail.com>
2018-10-04 14:04:03 -0500

    Update generated index file

--- src/ui/track/index.ts
+++ src/ui/track/index.ts
@@ -2,7 +2,7 @@
 
 export * from './util';
 export * from './AnnotationTrack';
-export * from './BaseTrack';
+export * from './TrackObject';
 export * from './ConstructTrack';
 export * from './IntervalTrack';
 export * from './SequenceTrack';

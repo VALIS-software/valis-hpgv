f24200a257b20e65ffc324e69d4b3481d9f75c83
George Corney <haxiomic@gmail.com>
2018-11-16 20:23:05 +0000

    Remove comment

--- src/track/signal/SignalTrackModel.ts
+++ src/track/signal/SignalTrackModel.ts
@@ -3,5 +3,5 @@ import { TrackModel } from "../TrackModel";
 export type SignalTrackModel = TrackModel & {
     readonly type: 'signal',
 
-    readonly path: string, // @! this is a temporary field until signal data is a core part of a dataSource
+    readonly path: string,
 }

752ccc3e751da31fa359c58b1ad3a607ee180eb7
George Corney <haxiomic@gmail.com>
2018-11-18 23:22:43 +0000

    Update generated

--- @types/track/annotation/AnnotationTrack.d.ts
+++ @types/track/annotation/AnnotationTrack.d.ts
@@ -2,6 +2,7 @@
 import UsageCache from "engine/ds/UsageCache";
 import InteractionEvent from "engine/ui/InteractionEvent";
 import Object2D from "engine/ui/Object2D";
+import Text from "engine/ui/Text";
 import IntervalInstances from "../../ui/util/IntervalInstances";
 import TrackObject from "../TrackObject";
 import { AnnotationTileLoader, Gene } from "./AnnotationTileLoader";
@@ -25,14 +26,20 @@ export declare class AnnotationTrack extends TrackObject<AnnotationTrackModel, A
     protected pointerState: TrackPointerState;
     constructor(model: AnnotationTrackModel);
     protected _macroTileCache: UsageCache<IntervalInstances>;
-    protected _annotationCache: UsageCache<Object2D>;
+    protected _annotationCache: UsageCache<{
+        gene: GeneAnnotation;
+        name: Text;
+    }>;
     protected _onStageAnnotations: UsageCache<Object2D>;
     protected updateDisplay(samplingDensity: number, continuousLodLevel: number, span: number, widthPx: number): void;
     protected updateMicroAnnotations(x0: number, x1: number, span: number, samplingDensity: number, continuousLodLevel: number, opacity: number): void;
     protected updateMacroAnnotations(x0: number, x1: number, span: number, samplingDensity: number, opacity: number): void;
     protected addAnnotation: (annotation: Object2D) => void;
     protected removeAnnotation: (annotation: Object2D) => void;
-    protected deleteAnnotation: (annotation: Object2D) => void;
+    protected deleteAnnotation: (annotation: {
+        gene: Object2D;
+        name: Text;
+    }) => void;
     protected annotationKey: (feature: {
         soClass: import("react").Key;
         name?: string;
@@ -44,4 +51,11 @@ export declare class AnnotationTrack extends TrackObject<AnnotationTrackModel, A
 declare type TrackPointerState = {
     pointerOver: boolean;
 };
+declare class GeneAnnotation extends Object2D {
+    readonly compact: boolean;
+    readonly gene: Gene;
+    opacity: number;
+    protected _opacity: number;
+    constructor(compact: boolean, gene: Gene, trackPointerState: TrackPointerState, onAnnotationClicked: (e: InteractionEvent, feature: GenomeFeature, gene: Gene) => void);
+}
 export default AnnotationTrack;

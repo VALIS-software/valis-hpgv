6b39c22d9499175cd26d0a059beff74bc4bfc9c4
George Corney <haxiomic@gmail.com>
2018-11-19 21:33:31 +0000

    Update generated

--- @types/track/interval/IntervalTrack.d.ts
+++ @types/track/interval/IntervalTrack.d.ts
@@ -7,11 +7,12 @@ import IntervalTileLoader, { IntervalTilePayload } from "./IntervalTileLoader";
 import { IntervalTrackModel } from "./IntervalTrackModel";
 export declare class IntervalTrack<Model extends IntervalTrackModel = IntervalTrackModel> extends TrackObject<Model, IntervalTileLoader> {
     readonly intervalColor: number[];
+    readonly yPadding: number;
     constructor(model: Model);
     protected _intervalTileCache: UsageCache<IntervalInstances>;
     protected _onStage: UsageCache<Object2D>;
     protected updateDisplay(samplingDensity: number, continuousLodLevel: number, span: number, widthPx: number): void;
-    protected displayTileNode(tile: Tile<IntervalTilePayload>, z: number, x0: number, span: number, continuousLodLevel: number): IntervalInstances;
+    protected displayTileNode(tile: Tile<IntervalTilePayload>, z: number, continuousLodLevel: number): IntervalInstances;
     protected createTileNode(tile: Tile<IntervalTilePayload>): IntervalInstances;
     protected createInstance(tilePayload: IntervalTilePayload, intervalIndex: number, relativeX: number, relativeW: number): IntervalInstance;
     protected removeTile: (tile: IntervalInstances) => void;

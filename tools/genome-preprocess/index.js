d2f4a47394a35f10837bd490d676b961318dc249
George Corney <haxiomic@gmail.com>
2018-11-20 21:32:29 +0000

    Whitespace

--- tools/genome-preprocess/index.js
+++ tools/genome-preprocess/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
 // execute Main
-const outDir = './_built'; // from tsconfig	
-const mainPath = 'tools/genome-preprocess/src/Main'; // relative to rootDir	
+const outDir = './_built'; // from tsconfig
+const mainPath = 'tools/genome-preprocess/src/Main'; // relative to rootDir
 require(`${outDir}/${mainPath}`); 

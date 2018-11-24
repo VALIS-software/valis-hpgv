0e2faa4c7ea64120a71969b946eb230997543b8e
George Corney <haxiomic@gmail.com>
2018-11-24 19:05:12 +0000

    Update TypeScript example

--- examples/typescript/src/App.ts
+++ examples/typescript/src/App.ts
@@ -1,22 +1,42 @@
 import { GenomeVisualizer } from "genome-visualizer";
 
-let browser = new GenomeVisualizer(
-    {
-        panels: [{ location: { contig: 'chr1', x0: 0, x1: 249e6 } }],
-        tracks: [{ name: 'GRCh38', type: 'sequence' }],
-    },
-    './manifest.json'
-);
+let visualizer = new GenomeVisualizer({
+    allowNewPanels: true,
+    panels: [
+        {
+            location: { contig: 'chr1', x0: 0, x1: 249e6 }
+        }
+    ],
+    tracks: [
+        {
+            name: 'DNA',
+            type: 'sequence',
+            path: 'https://s3-us-west-1.amazonaws.com/valis-file-storage/genome-data/GRCh38.vdna-dir',
+        },
+        {
+            name: 'Genes',
+            type: 'annotation',
+            compact: true,
+            path: 'https://s3-us-west-1.amazonaws.com/valis-file-storage/genome-data/GRCh38.92.vgenes-dir',
+        },
+        {
+            name: 'Cerebellum, DNase',
+            type: 'signal',
+            path: "https://www.encodeproject.org/files/ENCFF833POA/@@download/ENCFF833POA.bigWig",
+            heightPx: 150,
+        },
+    ],
+});
 
-let container = document.getElementById('root');
+let container = document.getElementById('container');
 
 window.addEventListener('resize', update);
 
-update();
-
 function update() {
-    browser.render({
+    visualizer.render({
         width: window.innerWidth,
         height: window.innerHeight
     }, container);
 }
+
+update();

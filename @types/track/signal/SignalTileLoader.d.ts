8c060bc912b7ace7a796addb659741e30153a848
George Corney <haxiomic@gmail.com>
2018-12-01 16:04:45 +0000

    Update generated

--- @types/track/signal/SignalTileLoader.d.ts
+++ @types/track/signal/SignalTileLoader.d.ts
@@ -18,26 +18,34 @@ declare type BlockPayload = {
     floatPacking: boolean;
     getTexture(device: GPUDevice): GPUTexture;
 };
+declare type BigWigLoader = {
+    header: HeaderData;
+    reader: BigWigReader;
+    lodMap: Array<number>;
+    lodZoomIndexMap: Array<number | null>;
+};
 export declare class SignalTileLoader extends TileLoader<SignalTilePayload, BlockPayload> {
     protected readonly dataSource: IDataSource;
     protected readonly model: SignalTrackModel;
     protected readonly contig: string;
     ready: boolean;
     readonly scaleFactor: number;
-    protected lodMap: Array<number>;
-    protected lodZoomIndexMap: Array<number | null>;
-    protected bigWigReader: BigWigReader;
+    protected bigWigLoader: BigWigLoader;
     protected _scaleFactor: number;
     protected _logarithmicDisplay: boolean;
     static cacheKey(model: SignalTrackModel): string;
     static requestIndex: number;
     constructor(dataSource: IDataSource, model: SignalTrackModel, contig: string);
     mapLodLevel(l: number): number;
+    protected _initializationPromise: Promise<void>;
+    protected initializationPromise(): Promise<void>;
     protected onReady(): void;
+    protected getBigWigLoader(path: string): Promise<BigWigLoader>;
     protected generateLodLookups(bigWigHeader: HeaderData): {
         lodMap: Array<number>;
         lodZoomIndexMap: Array<number>;
     };
+    protected getBigWigData(bigWigLoader: BigWigLoader, tile: Tile<SignalTilePayload>, buffer: Float32Array, nChannels: number, offset: number): Promise<Float32Array>;
     protected getTilePayload(tile: Tile<SignalTilePayload>): Promise<SignalTilePayload>;
     protected createBlockPayload(lodLevel: number, lodX: number, tileWidth: number, rows: number): BlockPayload;
     protected releaseBlockPayload(payload: BlockPayload): void;

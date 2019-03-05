c373c5c382c351db0b5dcacedb3e344fbd4fa40f
George Corney <haxiomic@gmail.com>
2019-03-05 19:39:06 +0000

    Tenative bigBED support groundwork

---
+++ src/formats/UCSCBig.ts
@@ -0,0 +1,192 @@
+import { BigWigReader, HeaderData } from "bigwig-reader";
+import { Tile } from "../track";
+
+export type BigLoader = {
+    header: HeaderData,
+    reader: BigWigReader,
+    lodMap: Array<number>,
+    lodZoomIndexMap: Array<number | null>,
+}
+
+export class UCSCBig {
+
+    /**
+     * Generate a BigWig loader instance for a given BigWig file path
+     */
+    private static _requestIndex = 0;
+    static getBigLoader(path: string): Promise<BigLoader> {
+        // we use a custom loader so we can explicitly disable caching (which with range requests is bug prone in many browsers)
+        let bigWigReader = new BigWigReader({
+            load: (start: number, size?: number) => {
+                return new Promise<ArrayBuffer>((resolve, reject) => {
+                    let request = new XMLHttpRequest();
+                    // disable caching (because of common browser bugs)
+                    request.open('GET', path + '?cacheAvoid=' + this._requestIndex++, true);
+                    request.setRequestHeader('Range', `bytes=${start}-${size ? start + size - 1 : ""}`);
+
+                    request.responseType = 'arraybuffer';
+                    request.onloadend = () => {
+                        if (request.status >= 200 && request.status < 300) {
+                            // success-like response
+                            resolve(request.response);
+                        } else {
+                            // error-like response
+                            reject(`HTTP request error: ${request.statusText} (${request.status})`);
+                        }
+                    }
+                    request.send();
+                });
+            }
+        });
+
+        return bigWigReader.getHeader().then((header) => {
+            let lookupTables = this.generateLodLookups(header);
+            return {
+                ...lookupTables,
+                header: header,
+                reader: bigWigReader,
+            };
+        });
+    }
+
+    /**
+     * Given a BigWig loader instance, load BigWig data to cover *tile* into texture ArrayBuffer *buffer*.
+     * Copies values into *targetChannel* assuming *nChannels* texture channels.
+     */
+    static getBigWigDataForTile(
+        bigWigLoader: BigLoader,
+        contig: string,
+        tile: Tile<any>,
+        buffer: Float32Array,
+        nChannels: number,
+        targetChannel: number
+    ): Promise<Float32Array> {
+        let zoomIndex = bigWigLoader.lodZoomIndexMap[tile.lodLevel];
+        let lodDensity = Math.pow(2, tile.lodLevel);
+
+        // @! use for normalization
+        // @! review floor in i0, i1
+
+        let dataPromise: Promise<Float32Array>;
+
+        if (zoomIndex !== null) {
+            // fetch from zoomed
+            dataPromise = bigWigLoader.reader.readZoomData(
+                contig,
+                tile.x,
+                contig,
+                tile.x + tile.span, // @! needs checking,
+                zoomIndex,
+            ).then((zoomData) => {
+                // fill buffer with zoom data regions
+                for (let entry of zoomData) {
+                    let x0 = entry.start - tile.x;
+                    let x1 = entry.end - tile.x;
+                    let i0 = Math.floor(x0 / lodDensity);
+                    let i1 = Math.floor(x1 / lodDensity);
+
+                    // fake norm
+                    let value = (entry.sumData / entry.validCount);
+
+                    for (let i = i0; i < i1; i++) {
+                        buffer[i * nChannels + targetChannel] = value;
+                    }
+                }
+
+                return buffer;
+            });
+        } else {
+            // fetch 'raw'
+            dataPromise = bigWigLoader.reader.readBigWigData(
+                contig,
+                tile.x,
+                contig,
+                tile.x + tile.span, // @! needs checking,
+            ).then((rawData) => {
+                for (let entry of rawData) {
+                    let x0 = entry.start - tile.x;
+                    let x1 = entry.end - tile.x;
+                    let i0 = Math.floor(x0);
+                    let i1 = Math.floor(x1);
+
+                    let value = entry.value;
+
+                    for (let i = i0; i < i1; i++) {
+                        if ((i < 0) || (i >= tile.lodSpan)) continue; // out of range
+                        buffer[i * nChannels + targetChannel] = value;
+                    }
+                }
+
+                return buffer;
+            });
+        }
+
+        return dataPromise;
+    }
+
+    /**
+     * Convert a BigWig zoom levels header into maps so we can lookup the zoom level for any given lod
+     */
+    protected static generateLodLookups(bigWigHeader: HeaderData): {
+        lodMap: Array<number>,
+        lodZoomIndexMap: Array<number>,
+    } {
+        let reductionLevelToLod = (reductionLevel: number) => Math.floor(Math.log2(reductionLevel));
+
+        let availableLods = bigWigHeader.zoomLevelHeaders.map((h) => reductionLevelToLod(h.reductionLevel));
+        availableLods = availableLods.sort((a, b) => a - b); // manual sort method so that javascript doesn't sort our numbers alphabetically X_X
+
+        // lod level 0 should always be available
+        if (availableLods[0] !== 0) availableLods.unshift(0);
+
+        let highestLod = availableLods[availableLods.length - 1];
+
+        // fill maps
+        let lodMap = new Array(highestLod);
+        let lodZoomIndexMap = new Array(highestLod);
+
+        const diffLowerLimit = 2;
+
+        for (let i = 0; i <= highestLod; i++) {
+
+            // find nearest lod either side of i
+            for (let j = 0; j < availableLods.length; j++) {
+                let l = availableLods[j];
+                if (l > i) { // we've found the upper lod
+                    let upperLod = l;
+                    let lowerLod = availableLods[j - 1];
+                    let diffLower = i - lowerLod;
+                    let diffUpper = upperLod - i;
+
+                    // pick closest lod
+                    // prevent picking lower-lod if the different is too great – this is to prevent performance issues displaying many tiles
+                    let bestLod = ((diffLower < diffUpper) && (diffLower <= diffLowerLimit)) ? lowerLod : upperLod;
+
+                    lodMap[i] = bestLod;
+                    break;
+                }
+            }
+
+            // we failed to find an upper lod therefore use highest lod
+            if (lodMap[i] === undefined) {
+                lodMap[i] = highestLod;
+            }
+
+
+            // find corresponding index for this lod
+            let zoomHeaderEntry = bigWigHeader.zoomLevelHeaders.find((h) => reductionLevelToLod(h.reductionLevel) === lodMap[i]);
+
+            if (zoomHeaderEntry == null) {
+                lodZoomIndexMap[i] = null;
+            } else {
+                lodZoomIndexMap[i] = zoomHeaderEntry.index;
+            }
+        }
+
+        return {
+            lodMap: lodMap,
+            lodZoomIndexMap: lodZoomIndexMap,
+        }
+    }
+
+}
--- src/track/annotation/AnnotationTileLoader.ts
+++ src/track/annotation/AnnotationTileLoader.ts
@@ -1,7 +1,9 @@
 import IDataSource from "../../data-source/IDataSource";
 import { Tile, TileLoader } from "../TileLoader";
-import { GeneInfo, GenomeFeature, GenomeFeatureType, Strand, TranscriptComponentClass, TranscriptComponentInfo, TranscriptInfo } from "./AnnotationTypes";
+import { GeneInfo, GenomeFeature, GenomeFeatureType, Strand, TranscriptComponentClass, TranscriptComponentInfo, TranscriptInfo, GeneClass, SoGeneClass } from "./AnnotationTypes";
 import TrackModel from "../TrackModel";
+import { UCSCBig, BigLoader } from "../../formats";
+import { BigBedData } from "bigwig-reader";
 
 // Tile payload is a list of genes extended with nesting
 export type Gene = GeneInfo & {
@@ -17,7 +19,146 @@ export type Transcript = TranscriptInfo & {
 
 type TilePayload = Array<Gene>;
 
-function transformAnnotations(flatFeatures: Array<GenomeFeature>) {
+enum AnnotationFormat {
+    ValisGenes,
+    BigBed,
+}
+
+export class AnnotationTileLoader extends TileLoader<TilePayload, void> {
+
+    protected annotationFileFormat?: AnnotationFormat = null;
+
+    readonly macroLod = 5;
+
+    protected readonly macroLodBlendRange = 2;
+    protected readonly macroLodThresholdLow = 7;
+    protected readonly macroLodThresholdHigh = this.macroLodThresholdLow + this.macroLodBlendRange;
+
+    static cacheKey(model: TrackModel): string {
+        return model.path;
+    }
+
+    constructor(
+        protected readonly dataSource: IDataSource,
+        protected readonly model: TrackModel,
+        protected readonly contig: string,
+        tileSize: number = 1 << 20
+    ) {
+        super(tileSize, 1);
+
+        // determine annotation file format
+        if (model.path != null) {
+            let fileType = model.path.substr(model.path.lastIndexOf('.') + 1).toLowerCase();
+
+            switch (fileType) {
+                case 'vgenes-dir':
+                    this.annotationFileFormat = AnnotationFormat.ValisGenes;
+                    break;
+                case 'bigbed':
+                case 'bbed':
+                case 'bb':
+                    this.annotationFileFormat = AnnotationFormat.BigBed;
+                    break;
+            }
+        }
+    }
+
+    mapLodLevel(l: number) {
+        if (l < this.macroLod) {
+            return 0;
+        } else {
+            return this.macroLod;
+        }
+    }
+
+    protected _bigLoaderPromise: Promise<BigLoader> = null;
+    protected getBigLoader() {
+        if (this._bigLoaderPromise == null) {
+            this._bigLoaderPromise = UCSCBig.getBigLoader(this.model.path);
+        }
+        return this._bigLoaderPromise
+    }
+
+    protected getTilePayload(tile: Tile<TilePayload>): Promise<TilePayload> | TilePayload {
+        let isMacro = tile.lodLevel >= this.macroLod;
+        if (this.model.path != null) {
+            switch (this.annotationFileFormat) {
+                case AnnotationFormat.ValisGenes: {
+                    // using path override
+                    return AnnotationTileLoader.loadValisGenesAnnotations(this.model.path, this.contig, tile.x, tile.span, isMacro).then(transformAnnotationsValisGene);
+                }
+                case AnnotationFormat.BigBed: {
+                    return this.getBigLoader().then(loader => {
+                        let zoomIndex: number | null  = loader.lodZoomIndexMap[tile.lodLevel];
+
+                        if (zoomIndex == null) {
+                            // @! request range is off by 1
+                            return loader.reader.readBigBedData(this.contig, tile.x, this.contig, tile.x + tile.span).then(transformAnnotationsBigBed);
+                        } else {
+                            console.log('using zoomIndex', zoomIndex);
+                            return []
+                        }
+                    });
+                }
+            }
+        } else {
+            return this.dataSource.loadAnnotations(this.contig, tile.x, tile.span, isMacro).then(transformAnnotationsValisGene);
+        }
+    }
+
+    static loadValisGenesAnnotations(
+        path: string,
+        contig: string,
+        startBaseIndex: number,
+        span: number,
+        macro: boolean,
+    ): Promise<TilePayload> {
+        let jsonPath = `${path}/${contig}${macro ? '-macro' : ''}/${startBaseIndex},${span}.json`;
+        return new Promise<TilePayload>((resolve, reject) => {
+            let request = new XMLHttpRequest();
+            // disable caching (because of common browser bugs)
+            request.open('GET', jsonPath, true);
+            request.responseType = 'json';
+            request.onloadend = () => {
+                if (request.status >= 200 && request.status < 300) {
+                    // success-like response
+                    resolve(request.response);
+                } else {
+                    // error-like response
+                    reject(`HTTP request error: ${request.statusText} (${request.status})`);
+                }
+            }
+            request.send();
+        });
+    }
+
+}
+
+function transformAnnotationsBigBed(dataset: Array<BigBedData>): TilePayload {
+    return dataset.map((data: BigBedData) => {
+        let gene: Gene = {
+            type: GenomeFeatureType.Gene,
+
+            name: data.name,
+
+            // @! these are off by 1!
+            startIndex: data.start,
+            length: data.end - data.start,
+
+            strand: data.strand as Strand,
+            class: GeneClass.Unspecified,
+            soClass: 'gene',
+            
+            transcriptCount: 0,
+            transcripts: [
+                
+            ],
+        };
+        return gene;
+    });
+}
+
+function transformAnnotationsValisGene(flatFeatures: Array<GenomeFeature>): TilePayload {
     // convert flat list of features into a nested structure which is easier to work with for rendering
     let payload: TilePayload = new Array();
     let activeGene: TilePayload[0];
@@ -95,103 +236,6 @@ function transformAnnotations(flatFeatures: Array<GenomeFeature>) {
     }
 
     return payload;
-}
-
-enum AnnotationFormat {
-    ValisGenes,
-    BigBed,
-}
-
-export class AnnotationTileLoader extends TileLoader<TilePayload, void> {
-
-    protected annotationFileFormat?: AnnotationFormat = null;
-
-    readonly macroLod = 5;
-
-    protected readonly macroLodBlendRange = 2;
-    protected readonly macroLodThresholdLow = 7;
-    protected readonly macroLodThresholdHigh = this.macroLodThresholdLow + this.macroLodBlendRange;
-
-    static cacheKey(model: TrackModel): string {
-        return model.path;
-    }
-
-    constructor(
-        protected readonly dataSource: IDataSource,
-        protected readonly model: TrackModel,
-        protected readonly contig: string,
-        tileSize: number = 1 << 20
-    ) {
-        super(tileSize, 1);
-
-        // determine annotation file format
-        if (model.path != null) {
-            let fileType = model.path.substr(model.path.lastIndexOf('.') + 1).toLowerCase();
-
-            switch (fileType) {
-                case 'vgenes-dir':
-                    this.annotationFileFormat = AnnotationFormat.ValisGenes;
-                    break;
-                case 'bigbed':
-                case 'bbed':
-                case 'bb':
-                    this.annotationFileFormat = AnnotationFormat.BigBed;
-                    break;
-            }
-        }
-    }
-
-    mapLodLevel(l: number) {
-        if (l < this.macroLod) {
-            return 0;
-        } else {
-            return this.macroLod;
-        }
-    }
-
-    protected getTilePayload(tile: Tile<TilePayload>): Promise<TilePayload> | TilePayload {
-        let isMacro = tile.lodLevel >= this.macroLod;
-        if (this.model.path != null) {
-            switch (this.annotationFileFormat) {
-                case AnnotationFormat.ValisGenes: {
-                    // using path override
-                    return AnnotationTileLoader.loadValisGenesAnnotations(this.model.path, this.contig, tile.x, tile.span, isMacro).then(transformAnnotations);
-                }
-                case AnnotationFormat.BigBed: {
-                    return [];
-                }
-            }
-        } else {
-            return this.dataSource.loadAnnotations(this.contig, tile.x, tile.span, isMacro).then(transformAnnotations);
-        }
-    }
-
-    static loadValisGenesAnnotations(
-        path: string,
-        contig: string,
-        startBaseIndex: number,
-        span: number,
-        macro: boolean,
-    ): Promise<TilePayload> {
-        let jsonPath = `${path}/${contig}${macro ? '-macro' : ''}/${startBaseIndex},${span}.json`;
-        return new Promise<TilePayload>((resolve, reject) => {
-            let request = new XMLHttpRequest();
-            // disable caching (because of common browser bugs)
-            request.open('GET', jsonPath, true);
-            request.responseType = 'json';
-            request.onloadend = () => {
-                if (request.status >= 200 && request.status < 300) {
-                    // success-like response
-                    resolve(request.response);
-                } else {
-                    // error-like response
-                    reject(`HTTP request error: ${request.statusText} (${request.status})`);
-                }
-            }
-            request.send();
-        });
-    }
-
 }
 
 export default AnnotationTileLoader;
